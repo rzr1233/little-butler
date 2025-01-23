92375d826ec1173b75fda43413f4188cdd3b6784
清香客 <809854538@qq.com>
2025-01-23 22:24:11 +0800

    修改成相对路径

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,8 +1,8 @@
 from django.apps import AppConfig
+from pathlib import Path
 
 
 class AccountsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "accounts"
     verbose_name = "用户管理"
-    path = "/home/zyn1233/little-butler/accounts"  # 显式指定路径
